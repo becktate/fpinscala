7460c4fc06c881f6e15a6077cfda4bcf79c9c96b
Becky Tate <mrtate@us.ibm.com>
2016-03-14 11:50:45 -0400

    Worked on exercises 7 and 8

--- exercises/src/main/scala/fpinscala/datastructures/Chapter3Tests.scala
+++ exercises/src/main/scala/fpinscala/datastructures/Chapter3Tests.scala
@@ -20,24 +20,36 @@ import List._;
     println("Exercise 2: tail(NIL) == NIL?  " + tail(List(Nil)))
     println("Exercise 2: tail(List(1, 2)) == List(2)?  " + tail(List(1, 2)))
     println("Exercise 2: tail(List(1, 2, 3)) == List(2, 3)?  " + tail(List(1, 2, 3)))
+    println()
 
     println("Exercise 3: setHead(NIL, 1) == Cons(1,NIL)?  " + setHead(List(Nil), 1))
     println("Exercise 3: setHead(List(1, 2), 3) == List(3, 2)?  " + setHead(List(1, 2), 3))
     println("Exercise 3: setHead(List(1, 2, 3), 4) == List(4, 2, 3)?  " + setHead(List(1, 2, 3), 4))
-
+    println()
 
     println("Exercise 4: drop(NIL, 1) == Nil?  " + drop(List(Nil), 1))
     println("Exercise 4: drop(List(1, 2, 3), 3) == Nil?  " + drop(List(1, 2, 3), 3))
     println("Exercise 4: drop(List(1, 2, 3), 2) == Cons(3, Nil)?  " + drop(List(1, 2, 3), 2))
+    println()
 
 
     println("Exercise 5: dropWhile(NIL, (x: Int) => x == 1) == Nil?  " + dropWhile(Nil, (x: Int) => x == 1))
     println("Exercise 5: dropWhile(List(1, 1, 2, 3), (x:Int) => x == 1) == List(2, 3)?  " + dropWhile(List(1, 1, 2, 3), (x: Int) => x == 1))
     println("Exercise 5: dropWhile(List(1, 2, 3), (x:Int) => x == 3) == List(1, 2, 3)?  " + dropWhile(List(1, 2, 3), (x: Int) => x == 3))
+    println()
 
     println("Exercise 6: init(NIL) == Nil?  " + init(Nil))
     println("Exercise 6: init(List(1, 2, 3)) == List(1, 2)?  " + init(List(1, 2, 3)))
     println("Exercise 6: init(List(1, 2, 3, 2) == List(1, 2, 3)?  " + init(List(1, 2, 3, 2)))
+    println()
+
+    println("Exercise 7: Nope, evaluates arguments first so it traverses the entire list")
+
+    println("Exercise 8: Expect just the same list to get returned...")
+    println("Exercise 8: what happens if you call foldright with Nils and Cons?  " + foldRight(List(1,2,3), Nil:List[Int])(Cons(_ , _)))
+    println()
+
+    println("Exercise 9: ")
   }
 }
 
